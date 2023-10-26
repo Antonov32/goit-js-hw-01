43f44f421d4cbbd82c113d70abc18c70e490da67
Antonov32 <112275153+Antonov32@users.noreply.github.com>
2023-10-26 22:11:26 +0200

    correct homework

--- js/task-2.js
+++ js/task-2.js
@@ -8,11 +8,6 @@
 
 // <country> — це країни доставки
 // <totalPrice> — це загальна вартість замовлення, що включає вартість товару і його доставки
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
-
-// console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-// console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-// console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
 
 function getShippingMessage(country, price, deliveryFee) {
   return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
